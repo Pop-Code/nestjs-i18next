9fb3492a012291fc1bb492fcc2de304ec8a0ccb9
Rmannn <alex.hermann@pop-code.com>
2022-04-26 21:07:56 +0200

    fix: options types

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,12 +1,16 @@
 import { FactoryProvider } from '@nestjs/common';
 import { i18n, InitOptions, Resource } from 'i18next';
 
-export type AsyncInitOptions = Omit<FactoryProvider<Partial<InitOptions> | i18n>, 'provide'>;
+export type AsyncInitOptions = Omit<
+    FactoryProvider<Partial<InitOptions> | i18n | Promise<Partial<InitOptions> | i18n>>,
+    'provide'
+>;
 
 export interface ForFeatureOptions {
     resources: Resource;
 }
 
-export interface AsyncForFeatureOptions extends Omit<FactoryProvider<ForFeatureOptions>, 'provide'> {
+export interface AsyncForFeatureOptions
+    extends Omit<FactoryProvider<ForFeatureOptions | Promise<ForFeatureOptions>>, 'provide'> {
     name: string;
 }
